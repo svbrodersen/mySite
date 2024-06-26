ccbd2512b42297dc90bf7da337989fa28e8c7b91
Simon Vinding Brodersen <simon@3450.dk>
2024-06-26 12:29:25 +0200

    Update to handlers

--- cmd/main.go
+++ cmd/main.go
@@ -5,12 +5,13 @@ import (
 	"net/http"
 
 	"github.com/a-h/templ"
+	"github.com/svbrodersen/mySite/handlers"
 	"github.com/svbrodersen/mySite/views/templates"
 )
 
 func main() {
 	component := templates.Hello("Simon")
-	http.Handle("/", templ.Handler(component))
+	http.Handle("/", handlers.indexHandler)
 	fmt.Println("Listening on :3333")
 	http.ListenAndServe(":3333", nil)
 }
