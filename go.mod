a1ee8782552f014263c8bf0b86c6b4c3630540f0
Simon Vinding Brodersen <simon@3450.dk>
2024-06-29 19:36:42 +0200

    Working on contact form

--- go.mod
+++ go.mod
@@ -2,4 +2,14 @@ module github.com/svbrodersen/mySite
 
 go 1.22.0
 
-require github.com/a-h/templ v0.2.707
+require (
+	github.com/a-h/templ v0.2.707
+	github.com/go-playground/validator v9.31.0+incompatible
+)
+
+require (
+	github.com/go-playground/locales v0.14.1 // indirect
+	github.com/go-playground/universal-translator v0.18.1 // indirect
+	github.com/leodido/go-urn v1.4.0 // indirect
+	gopkg.in/go-playground/assert.v1 v1.2.1 // indirect
+)
--- handlers/contact.go
+++ handlers/contact.go
@@ -1,10 +1,39 @@
 package handlers
 
 import (
-	"github.com/svbrodersen/mySite/views/templates"
+	"encoding/json"
+	"fmt"
 	"net/http"
-	"time"
+	"net/smtp"
+	"os"
+
+	"github.com/go-playground/validator"
+	"github.com/svbrodersen/mySite/views/components"
+	"github.com/svbrodersen/mySite/views/templates"
 )
+
+type ContactForm struct {
+	Name    string `json:"Name" validate:"required"`
+	Email   string `json:"Email" validate:"required, email"`
+	Subject string `json:"Subject" validate:"required"`
+	Body    string `json:"Body" validate:"required"`
+}
+
+func send_mail_to_self(form *ContactForm) error {
+	from := "simon@3450.dk"
+	password := os.Getenv("EMAIL_PASSWORD")
+
+	to := []string{from}
+
+	smtpHost := "smtp.gmail.com"
+	smtpPort := "587"
+
+	message := []byte("This is a test email")
+	auth := smtp.PlainAuth("", from, password, smtpHost)
+
+	err := smtp.SendMail(fmt.Sprintf("%s:%s", smtpHost, smtpPort), auth, from, to, message)
+	return err
+}
 
 func ContactHandler(w http.ResponseWriter, r *http.Request) {
 	if r.Method == "PATCH" {
@@ -12,6 +41,23 @@ func ContactHandler(w http.ResponseWriter, r *http.Request) {
 	} else if r.Method == "GET" {
 		templates.Contact(false).Render(r.Context(), w)
 	} else if r.Method == "POST" {
-		time.Sleep(2 * time.Second)
+		var contact_form ContactForm
+		err := json.NewDecoder(r.Body).Decode(&contact_form)
+		if err != nil {
+			http.Error(w, "Invalid input", http.StatusBadRequest)
+			return
+		}
+		validate := validator.New()
+		err = validate.Struct(contact_form)
+		if err != nil {
+			errors := err.(validator.ValidationErrors)
+			http.Error(w, fmt.Sprintf("Validation error: %s", errors), http.StatusBadRequest)
+			return
+		}
+		err = send_mail_to_self(&contact_form)
+		if err != nil {
+			http.Error(w, "Unable to send email to myself", http.StatusBadRequest)
+		}
+		components.ContactSuccess().Render(r.Context(), w)
 	}
 }
